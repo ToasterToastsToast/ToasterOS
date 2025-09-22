867966e25f051e0744fec5dc3fd70dc084db21ca
ToasterToastsToast <zhourjr2021@163.com>
2025-09-22 14:15:48 +0800

    st1

--- src/kernel/boot/start.c
+++ src/kernel/boot/start.c
@@ -22,7 +22,7 @@ void start()
     w_mstatus(status);
 
     // 设置M-mode的返回地址
-
-    // 触发状态迁移，回到上一个状态（M-mode->S-mode）
-
+    w_mepc((uint64)main);
+    // 触发状态迁移，回到上一个状态
+    asm volatile("mret");//从xv6抄的，M-mode RETurn
 }
